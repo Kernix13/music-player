fcdce271a343ee23c14bf39916b2c3bff94f2f00
Jim <jimkernicky@gmail.com>
2021-05-30 09:18:55 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,125 @@
+const musicContainer = document.getElementById('music-container');
+
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+
+// const cover = document.getElementById('cover');
+
+// Song Titles
+const songs = [
+  'House of the Risin Sun',
+  'Little Paradise',
+  'Molasses',
+  'Moon', 
+  'Summer',
+  'Ukulele',
+  'Hey',
+];
+
+// Keep track of song
+let songIndex = 0;
+
+// initially load song details into DOM
+loadSong(songs[songIndex]);
+
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    // cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+  console.log('Working');
+}
+
+// set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  // console.log('Working');
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// volume controls
+
+
+// Event Listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change Song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// time and song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// click on progrss bar
+progressContainer.addEventListener('click', setProgress);
+
+// song ends
+audio.addEventListener('ended', nextSong);
+
