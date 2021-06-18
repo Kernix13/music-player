af9a127d7737c58d083de90697214b2fbb4f129c
Jim <jimkernicky@gmail.com>
2021-06-18 11:31:53 -0400

    Fixed volume, added time update

--- readme.txt
+++ readme.txt
@@ -1,1 +1,9 @@
-You have to change the src for the music files - my code has references to my music file in local dev.
+1. You have to change the src for the music files - my code has references to music file on my local machine. Test it out with this src on line 41 of script.js:
+https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3
+ In place of 
+ audio.src = `music/${song}.mp3` try   
+ audio.src = `https://raw.githubusercontent.com/himalayasingh/music-player-1/master/music/4.mp3`;
+
+2. Originally the DOM elements of .music-container and #title would only show while the song was playing. I changed that in the CSS file because I wanted that info up whether it was playing, paused, or stopped. Search for .music-info in style.css.
+
+3. I had to remove the song duration because NaN kept displaying before the duration loaded.
--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const musicContainer = document.getElementById('music-container');
+const audioPlayer = musicContainer.querySelector('.audio_player');
 
-const playBtn = document.getElementById('play');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
+const title = document.getElementById('title');
+// 
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+// 
+// const progressContainer = document.getElementById('progress-container');
+// const progress = document.getElementById('progress');
+
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
 
 const audio = document.getElementById('audio');
-const progress = document.getElementById('progress');
-const progressContainer = document.getElementById('progress-container');
-const title = document.getElementById('title');
+
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+const speaker = musicContainer.querySelector('.speaker');
+const speakerIcon = musicContainer.querySelector('#speaker_icon');
+
+const ranges = musicContainer.querySelectorAll('.player_slider');
+const volInput = musicContainer.querySelector('input[name="volume"]')
 
 // const cover = document.getElementById('cover');
 
@@ -34,7 +49,7 @@ function loadSong(song) {
   // cover.src = `images/${song}.jpg`;
 }
 
-// Play song
+// remove Play icon, add Pause icon 
 function playSong() {
   musicContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.remove('fa-play');
@@ -43,7 +58,8 @@ function playSong() {
   audio.play();
 }
 
-// Pause song
+// remove Pause icon, add Play icon
+// why aren't these combined into a play\pause toggle function?
 function pauseSong() {
   musicContainer.classList.remove('play');
   playBtn.querySelector('i.fas').classList.add('fa-play');
@@ -79,25 +95,96 @@ function nextSong() {
 }
 
 // update progress bar
-function updateProgress(e) {
-  const { duration, currentTime } = e.srcElement;
-  const progressPercent = (currentTime / duration) * 100;
-  progress.style.width = `${progressPercent}%`;
-  console.log('Working');
+// function updateProgress(e) {
+//   const { duration, currentTime } = e.srcElement;
+//   const progressPercent = (currentTime / duration) * 100;
+//   progress.style.width = `${progressPercent}%`;
+//   console.log('Working');
+// }
+
+/* updateProgress - the above one stops progress, the one below works but get NaN */
+
+
+// VIDEO - - - - - - -Calculate display time format
+// IT'S THIS FUNCTION, THE SECONDS
+
+function displayTime(time) {
+  const minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+  seconds = seconds > 9 ? seconds : `0${seconds}`;
+  return `${minutes}:${seconds}`;
+  // return minutes + ':' + seconds;
+}
+
+// VIDEO - - - - - - - update progress bar as the video plays
+// OR THIS FUNCTION, DURATION IS A PROPERTY OF THE VIDEO ELEMENT, WHAT ABOUT THE AUDIO ELEMENT? ad to comment out time-duration in index.html
+
+function updateProgress() {
+  progressBar.style.width = `${(audioPlayer.currentTime / audioPlayer.duration) * 100}%`;
+  // currentTime.textContent = `${displayTime(audioPlayer.currentTime)} /`;
+  currentTime.textContent = `${displayTime(audioPlayer.currentTime)}`;
+  duration.textContent = `${displayTime(audioPlayer.duration)}`;
+}
+// VIDEO - - - - -
+function scrub(event) {
+  const scrubTime = (event.offsetX / progressRange.offsetWidth) * audioPlayer.duration;
+  audioPlayer.currentTime = scrubTime;
+}
+
+// from https://css-tricks.com/lets-create-a-custom-audio-player/ but only got rid of the initial load - search: html5 audio element duration nan
+const displayDuration = () => {
+  duration.textContent = displayTime(audio.duration);
+}
+
+if (audio.readyState > 0) {
+  displayDuration();
+} else {
+  audio.addEventListener('loadedmetadata', () => {
+    displayDuration();
+  });
 }
 
 // set progress bar
-function setProgress(e) {
-  const width = this.clientWidth;
-  const clickX = e.offsetX;
-  const duration = audio.duration;
-  // console.log('Working');
+// function setProgress(e) {
+//   const width = this.clientWidth;
+//   const clickX = e.offsetX;
+//   const duration = audio.duration;
 
-  audio.currentTime = (clickX / width) * duration;
+//   audio.currentTime = (clickX / width) * duration;
+// }
+// VIDEO - - - - - Click to seek within the video
+function setProgress(e) {
+  const newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  audioPlayer.currentTime = newTime * audioPlayer.duration;
 }
 
-// volume controls
+// ==================== VOLUME CONTROLS
 
+// volume functions
+function handleRangeUpdate() {
+  audioPlayer[this.name] = this.value;
+  (audioPlayer['volume'] === 0 ? speakerIcon.className = "fa fa-volume-off" :
+    speakerIcon.className = "fa fa-volume-up")
+}
+
+let muted = false;
+
+function mute() {
+  if (!muted) {
+    audioPlayer['volume'] = 0;
+    volInput.value = 0;
+    speakerIcon.className = "fa fa-volume-off"
+    muted = true;
+  } else {
+    audioPlayer['volume'] = 1;
+    volInput.value = 1;
+    muted = false;
+    speakerIcon.className = "fa fa-volume-up"
+  }
+}
+
+// ==================== END VOLUME CONTROLS
 
 // Event Listeners
 playBtn.addEventListener('click', () => {
@@ -118,8 +205,24 @@ nextBtn.addEventListener('click', nextSong);
 audio.addEventListener('timeupdate', updateProgress);
 
 // click on progrss bar
-progressContainer.addEventListener('click', setProgress);
+// progressContainer.addEventListener('click', setProgress);
+// VIDEO
+progressRange.addEventListener('click', setProgress);
+
+// preload metadata
+
 
 // song ends
 audio.addEventListener('ended', nextSong);
 
+// volume
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+speaker.addEventListener('click', mute)
+
+// progress bar controls
+let mouseDown = false;
+progressRange.addEventListener('click', scrub);
+progressRange.addEventListener('mousemove', (event) => mouseDown && scrub(event));
+progressRange.addEventListener('mousedown', () => mouseDown = true);
+progressRange.addEventListener('mouseup', () => mouseDown = false);
